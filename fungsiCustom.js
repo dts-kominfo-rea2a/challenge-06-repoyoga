7937802009907666d3b3dbea243330299ec2ef5c
Restiawan Yoga Pratama <restiawan008@gmail.com>
2022-06-08 22:48:16 +0700

    tantangan callback algo, commit pertama

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,41 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  arrData = []
+
+  fs.readFile(file1, 'utf8', (err, data) => {
+    if (err) {
+      return fnCallback(err, null)
+    } else {
+      let dataparse = JSON.parse(data);
+      let hasil = dataparse.message.split(' ');
+      arrData.push(hasil[1])
+
+      fs.readFile(file2, 'utf8', (err, data) => {
+        if (err) {
+          return fnCallback(err, null)
+        } else {
+          let dataparse = JSON.parse(data);
+          let hasil = dataparse[0].message.split(' ');
+          arrData.push(hasil[1])
+
+          fs.readFile(file3, 'utf8', (err, data) => {
+            if (err) {
+              return fnCallback(err, null)
+            } else {
+              let dataparse = JSON.parse(data);
+              let hasil = dataparse[0].data.message.split(' ');
+              arrData.push(hasil[1])
+              
+              return fnCallback(err, arrData)
+            }
+          });
+        }
+      });
+    }     
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
